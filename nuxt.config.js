dc29b7167b1d9c928f84007f9f4f44a9d944d204
John Cosio <mail@jcos.io>
2020-11-08 01:26:28 +0800

    Disabling proxyHeaders to work on nginx reverse proxy.
    https://github.com/nuxt/nuxt.js/issues/1999#issuecomment-341084986

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,9 @@ export default {
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: {
+    proxyHeaders: false
+  },
 
   // Content module configuration (https://go.nuxtjs.dev/config-content)
   content: {},
