f25058e00d59895c3fb16fef275f57bc1f60f5d2
John Cosio <mail@jcos.io>
2020-11-08 00:18:08 +0800

    Adding the tic tac toe component.
    Also updated the main page

---
+++ components/Board/Game.vue
@@ -0,0 +1,235 @@
+<template>
+  <div id="app" class="m-4">
+    <div class="status mb-2 text-2xl">
+      <h3>{{ win ? win : status }}</h3>
+    </div>
+    <div class="bg-white">
+      <template v-for="row in 3">
+        <div :key="row" class="row">
+          <button
+            v-for="button in 3"
+            :id="row.toString() + button.toString()"
+            :key="button"
+            v-bind="box.current[button]"
+            class="square bg-green-200 hover:bg-green-400 rounded m-3"
+            style="height: 60px; width: 60px"
+            @click="play(row, button)"
+          >
+            <span class="text-green-200">.</span>
+          </button>
+        </div>
+      </template>
+      <button class="button--grey my-2" @click="reset">Reset</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      win: '',
+      status: 'Current player: X',
+      player: 'X',
+      box: {
+        current: [],
+      },
+      turns: [],
+      gameOver: false,
+    }
+  },
+  methods: {
+    play(row, button) {
+      const selected = {}
+      selected[row.toString() + button.toString()] = this.player
+
+      this.turns.push(selected)
+      document.getElementById(
+        row.toString() + button.toString()
+      ).textContent = this.player
+      document
+        .getElementById(row.toString() + button.toString())
+        .setAttribute('disabled', 'disabled')
+
+      this.didPlayerWin()
+
+      if (!this.gameOver) {
+        if (this.player === 'X') {
+          this.player = 'O'
+          this.status = 'Current player: O'
+        } else {
+          this.player = 'X'
+          this.status = 'Current player: X'
+        }
+      }
+    },
+    didPlayerWin() {
+      if (this.turns.length > 4) {
+        this.winConditions(this.player)
+      }
+    },
+    winConditions(player) {
+      const condition1 = ['11', '12', '13']
+      const condition2 = ['21', '22', '23']
+      const condition3 = ['31', '32', '33']
+      const condition4 = ['11', '21', '31']
+      const condition5 = ['12', '22', '32']
+      const condition6 = ['13', '23', '33']
+      const condition7 = ['11', '22', '33']
+      const condition8 = ['13', '22', '31']
+      const playerTurns = []
+      const turns = this.turns
+      Object.keys(turns).forEach(function (item) {
+        Object.keys(turns[item]).forEach(function (value, key) {
+          if (turns[item][value] === player) {
+            playerTurns.push(value)
+          }
+        })
+      })
+
+      // Check if player wins
+      if (this.isMatchingWinCondition(condition1, playerTurns.sort())) {
+        this.win = player + ' wins!'
+        this.gameOver = true
+      }
+
+      if (this.isMatchingWinCondition(condition2, playerTurns.sort())) {
+        this.win = player + ' wins!'
+        this.gameOver = true
+      }
+
+      if (this.isMatchingWinCondition(condition3, playerTurns.sort())) {
+        this.win = player + ' wins!'
+        this.gameOver = true
+      }
+
+      if (this.isMatchingWinCondition(condition4, playerTurns.sort())) {
+        this.win = player + ' wins!'
+        this.gameOver = true
+      }
+
+      if (this.isMatchingWinCondition(condition5, playerTurns.sort())) {
+        this.win = player + ' wins!'
+        this.gameOver = true
+      }
+
+      if (this.isMatchingWinCondition(condition6, playerTurns.sort())) {
+        this.win = player + ' wins!'
+        this.gameOver = true
+      }
+
+      if (this.isMatchingWinCondition(condition7, playerTurns.sort())) {
+        this.win = player + ' wins!'
+        this.gameOver = true
+      }
+
+      if (this.isMatchingWinCondition(condition8, playerTurns.sort())) {
+        this.win = player + ' wins!'
+        this.gameOver = true
+      }
+
+      if (this.gameOver) {
+        document.getElementById('11').setAttribute('disabled', 'disabled')
+        document.getElementById('12').setAttribute('disabled', 'disabled')
+        document.getElementById('13').setAttribute('disabled', 'disabled')
+        document.getElementById('21').setAttribute('disabled', 'disabled')
+        document.getElementById('22').setAttribute('disabled', 'disabled')
+        document.getElementById('23').setAttribute('disabled', 'disabled')
+        document.getElementById('31').setAttribute('disabled', 'disabled')
+        document.getElementById('32').setAttribute('disabled', 'disabled')
+        document.getElementById('33').setAttribute('disabled', 'disabled')
+      }
+    },
+    isMatchingWinCondition(arr, arr2) {
+      return arr.every((i) => arr2.includes(i))
+    },
+    reset() {
+      this.status = 'Current player: X'
+      this.player = 'X'
+      this.turns = []
+      this.gameOver = false
+      this.win = null
+      document.getElementById('11').textContent = ''
+      document
+        .getElementById('11')
+        .insertAdjacentHTML(
+          'beforeend',
+          '<span class="text-green-200">.</span>'
+        )
+      document.getElementById('11').removeAttribute('disabled')
+      document.getElementById('12').textContent = ''
+      document
+        .getElementById('12')
+        .insertAdjacentHTML(
+          'beforeend',
+          '<span class="text-green-200">.</span>'
+        )
+      document.getElementById('12').removeAttribute('disabled')
+      document.getElementById('13').textContent = ''
+      document
+        .getElementById('13')
+        .insertAdjacentHTML(
+          'beforeend',
+          '<span class="text-green-200">.</span>'
+        )
+      document.getElementById('13').removeAttribute('disabled')
+
+      document.getElementById('21').textContent = ''
+      document
+        .getElementById('21')
+        .insertAdjacentHTML(
+          'beforeend',
+          '<span class="text-green-200">.</span>'
+        )
+      document.getElementById('21').removeAttribute('disabled')
+      document.getElementById('22').textContent = ''
+      document
+        .getElementById('22')
+        .insertAdjacentHTML(
+          'beforeend',
+          '<span class="text-green-200">.</span>'
+        )
+      document.getElementById('22').removeAttribute('disabled')
+      document.getElementById('23').textContent = ''
+      document
+        .getElementById('23')
+        .insertAdjacentHTML(
+          'beforeend',
+          '<span class="text-green-200">.</span>'
+        )
+      document.getElementById('23').removeAttribute('disabled')
+
+      document.getElementById('31').textContent = ''
+      document
+        .getElementById('31')
+        .insertAdjacentHTML(
+          'beforeend',
+          '<span class="text-green-200">.</span>'
+        )
+      document.getElementById('31').removeAttribute('disabled')
+      document.getElementById('32').textContent = ''
+      document
+        .getElementById('32')
+        .insertAdjacentHTML(
+          'beforeend',
+          '<span class="text-green-200">.</span>'
+        )
+      document.getElementById('32').removeAttribute('disabled')
+      document.getElementById('33').textContent = ''
+      document
+        .getElementById('33')
+        .insertAdjacentHTML(
+          'beforeend',
+          '<span class="text-green-200">.</span>'
+        )
+      document.getElementById('33').removeAttribute('disabled')
+    },
+  },
+}
+</script>
+
+<style>
+.square:hover > span {
+  color: #68d391;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,33 +2,39 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">practice-nuxt-tic-tac-toe</h1>
+      <h1 class="text-3xl title">practice-nuxt-tic-tac-toe</h1>
+      <Game />
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://github.com/sudoist/practice-nuxt-tic-tac-toe"
           target="_blank"
           rel="noopener noreferrer"
           class="button--grey"
         >
           GitHub
         </a>
+        <a
+          href="https://codepen.io/sudoist/pen/VwjGrbL"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          CodePen
+        </a>
       </div>
-      <h4>
+      <h4 class="mt-3 text-lg">
         This started as a Vue.js technical exam but I went and tested out
-        Nuxt.js as well. Original app can be viewed at
-        <a href="https://codepen.io/sudoist/pen/VwjGrbL"></a>
+        Nuxt.js as well.
       </h4>
-      <p>
-        Here is a random component to show some random facts on page load from
-        <a href="https://uselessfacts.jsph.pl/">
+      <h4>
+        Original app can be viewed at
+        <a class="text-blue-700" href="https://codepen.io/sudoist/pen/VwjGrbL">
+          https://codepen.io/sudoist/pen/VwjGrbL
+        </a>
+      </h4>
+      <p class="mt-3">
+        Below is a random component to show a random fact on page load from
+        <a class="text-blue-700" href="https://uselessfacts.jsph.pl/">
           https://uselessfacts.jsph.pl/
         </a>
         to practice API fetch.
@@ -66,9 +72,15 @@ export default {
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 70px;
   color: #35495e;
   letter-spacing: 1px;
+}
+
+@media only screen and (max-width: 600px) {
+  .title {
+    font-size: 2.5rem;
+  }
 }
 
 .subtitle {
